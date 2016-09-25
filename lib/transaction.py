237fdcc4319a3d4731e3f447978d853c90a1ba26
sunerok <justinvforvendetta@gmail.com>
2016-09-25 02:06:42 -0400

    fix

--- lib/transaction.py
+++ lib/transaction.py
@@ -577,7 +577,7 @@ class Transaction:
                 script = '76a9'                                      # op_dup, op_hash_160
                 script += push_script(hash_160.encode('hex'))
                 script += '88ac'                                     # op_equalverify, op_checksig
-            elif addrtype == 76:
+            elif addrtype == 3:
                 script = '3'                                        # op_hash_160
                 script += push_script(hash_160.encode('hex'))
                 script += '87'                                       # op_equal
